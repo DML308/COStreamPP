766be97d7441632135cce9db3f6d719764ef9a53
yf <372874816@qq.com>
2018-12-25 11:15:54 +0800

    初态调度只计算init部分工作量,稳态调度需要计算statment_list和work函数的工作量

--- src/include/LifeCycle/workEstimate.h
+++ src/include/LifeCycle/workEstimate.h
@@ -1,3 +1,12 @@
+#ifndef _WORK_ESTIMATE_H
+#define _WORK_ESTIMATE_H
 #include "node.h"
-int workEstimate_init(operBodyNode *body);
-int workEstimate_steady(operBodyNode *body);
+#define INIT 0
+#define STEADY 1
+const int updateBuffer = 20;
+int work;
+int state; /*标识init和steady*/
+int workEstimate_init(operBodyNode *body, int w_init);
+int workEstimate_steady(operBodyNode *body, int w_steady);
+void workCompute(Node *node);
+#endif
--- src/src/FrontEnd/WorkEstimate.cpp
+++ src/src/FrontEnd/WorkEstimate.cpp
@@ -1,6 +1,6 @@
 #include "staticStreamGragh.h"
 #include "workEstimate.h"
-const int updateBuffer=20;
+
 void WorkEstimate(StaticStreamGraph *ssg)
 {
     int len = ssg->flatNodes.size();
@@ -12,8 +12,8 @@ void WorkEstimate(StaticStreamGraph *ssg)
         operBodyNode *body = flatNode->contents->operBody;
         if (body != NULL)
         {
-            w_init = workEstimate_init(body);
-            w_steady = workEstimate_steady(body);
+            w_init = workEstimate_init(body, w_init);
+            w_steady = workEstimate_steady(body, w_steady);
         }
         w_steady += (flatNode->outFlatNodes.size() + flatNode->inFlatNodes.size()) * updateBuffer; //多核下调整缓冲区head和tail
         ssg->AddInitWork(flatNode, w_init);
@@ -21,14 +21,101 @@ void WorkEstimate(StaticStreamGraph *ssg)
     }
 }
 
-int workEstimate_init(operBodyNode *body)
+/* 初始化初态调度工作量 */
+int workEstimate_init(operBodyNode *body, int w_init)
 {
-    int totalWork = 0;
-    return totalWork;
+    state = INIT;
+    work = w_init;
+    workCompute(body);
+    return work;
+}
+/* 初始化稳态调度工作量 */
+int workEstimate_steady(operBodyNode *body, int w_steady)
+{
+    state = STEADY;
+    work = w_steady;
+    workCompute(body);
+    return work;
 }
 
-int workEstimate_steady(operBodyNode *body)
+void workCompute(Node *node)
 {
-    int totalWork = 0;
-    return totalWork;
+    switch (node->type)
+    {
+    case OperBody: 
+        /* 初态调度只计算init部分工作量 */
+        if (state == INIT)
+        {
+            if (static_cast<operBodyNode *>(node)->init != NULL)
+            {
+                workCompute(static_cast<operBodyNode *>(node)->init);
+            }
+        }
+        /* 稳态调度需要计算statment_list和work函数的工作量 */
+        else if (state == STEADY)
+        {
+            if (static_cast<operBodyNode *>(node)->stmt_list != NULL)
+            {
+                for (auto it : *static_cast<operBodyNode *>(node)->stmt_list)
+                {
+                    workCompute(it);
+                }
+            }
+            if (static_cast<operBodyNode *>(node)->work != NULL)
+            {
+                workCompute(static_cast<operBodyNode *>(node)->work);
+            }
+        }
+        break;
+    case Comma:
+        break;
+    case Array:
+        break;
+    case Ternary:
+        break;
+    case Block:
+        break;
+    case FuncDcl:
+        break;
+    case Decl:
+        break;
+    case Id:
+        break;
+    case Unary:
+        break;
+    case Binop:
+        break;
+    case For:
+        break;
+    case If:
+        break;
+    case IfElse:
+        break;
+    case Do:
+        break;
+    case While:
+        break;
+    case Cast:
+        break;
+    case Switch:
+        break;
+    case Case:
+        break;
+    case Default:
+        break;
+    case Break:
+        break;
+    case Continue:
+        break;
+    case Call:
+        break;
+    case constant:
+        break;
+    case Initializer:
+        break;
+    case ImplicitCast:
+        break;
+    default:
+        break;
+    }
 }
